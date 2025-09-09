dadc7403f7d971aad4b1fa2b4b178850c74978db
Sohaib2github <sohaib.belaroussi.2@gmail.com>
2025-09-09 12:47:07 +0300

    created mcp1

---
+++ src/app/api/mcp1/route.ts
@@ -0,0 +1,49 @@
+import { createMcpHandler } from "mcp-handler";
+import { z } from "zod";
+
+// StreamableHttp server
+const handler = createMcpHandler(
+  async (server) => {
+    server.tool(
+      "get_weather",
+      "Get the current weather for a specified city",
+      {
+        city: z.string(),
+      },
+      async ({ city }) => ({
+        content: [{ type: "text", text: `The weather in ${city} is sunny and bright` }],
+      }),
+    );
+
+    server.tool(
+      "get_time",
+      "Get the current time for a specified city",
+      {
+        city: z.string(),
+      },
+      async ({ city }) => ({
+        content: [{ type: "text", text: `The time in ${city} is 5 o'clock` }],
+      }),
+    );
+  },
+  {
+    capabilities: {
+      tools: {
+        get_weather: {
+          description: "Get the current weather for a specified city",
+        },
+        get_time: {
+          description: "Get the current time for a specified city",
+        },
+      },
+    },
+  },
+  {
+    basePath: "",
+    verboseLogs: true,
+    maxDuration: 60,
+    disableSse: true,
+  },
+);
+
+export { handler as GET, handler as POST, handler as DELETE };
---
+++ test_mcp_streamablehttp.js
@@ -0,0 +1,24 @@
+    import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js";
+
+const origin = process.argv[2] || "http://localhost:3000/api/mcp1/mcp";
+
+async function main() {
+  const transport = new StreamableHTTPClientTransport(new URL(`${origin}/mcp`));
+
+  const client = new Client(
+    {
+      name: "example-client",
+      version: "1.0.0",
+    },
+  );
+
+  await client.connect(transport);
+
+  console.log("Connected", client.getServerCapabilities());
+
+  const result = await client.listTools();
+  console.log(result);
+}
+
+main();
